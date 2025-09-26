14db96d83315f82bd967761ab31963b431a85821
unknown <Aslih Maulana Syadat>
2025-09-26 22:09:47 +0700

    Modul 1 Selesai

--- Modul 1/24-166_Aslih_Maulana_Syadat/biodata.php
+++ Modul 1/24-166_Aslih_Maulana_Syadat/biodata.php
@@ -15,33 +15,36 @@ $hobi = "Bermain Game";
     <title>Biodata</title>
 </head>
 <body>
-    <table border="1" align="center" width="100%">
+    <h1>Biodata Diri</h1>
+    <table border="0">
         <tr>
-            <th>Field</th>
-            <th>Value</th>
-        </tr>
-        <tr>
-            <td>Nama Lengkap: </td>
+            <td>Nama Lengkap</td>
+            <td>:</td>
             <td><?= $nama ?></td>
         </tr>
         <tr>
-            <td>NIM: </td>
+            <td>NIM</td>
+            <td>:</td>
             <td><?= $NIM ?></td>
         </tr>
         <tr>
-            <td>Kelas: </td>
+            <td>Kelas</td>
+            <td>:</td>
             <td><?= $kelas ?></td>
         </tr>
         <tr>
-            <td>Program Studi: </td>
+            <td>Program Studi</td>
+            <td>:</td>
             <td><?= $prodi ?></td>
         </tr>
         <tr>
-            <td>Alamat: </td>
+            <td>Alamat</td>
+            <td>:</td>
             <td><?= $alamat ?></td>
         </tr>
         <tr>
-            <td>Hobi: </td>
+            <td>Hobi</td>
+            <td>:</td>
             <td><?= $hobi ?></td>
         </tr>
     </table>
